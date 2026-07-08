80eadb6d1364cba093f9ffd20f76f2655b883e58
codychun <codyc2023.red@gmail.com>
2026-07-07 23:24:58 -0400

    ALU Operations

--- include/riscv_pkg.sv
+++ include/riscv_pkg.sv
@@ -1,4 +1,4 @@
-// Instruction Opcode Constants
+// Instruction Field Constants
 
 package riscv_pkg;
     typedef enum logic [6:0] {
@@ -11,23 +11,31 @@ package riscv_pkg;
     } opcode_t;
 
     typedef enum logic [2:0] {
-        FUNC3_LW   = 3'b010;
-        FUNC3_ADDI = 3'b000;
-        FUNC3_SW   = 3'b010;
-        FUNC3_ADD  = 3'b000;
-        FUNC3_SUB  = 3'b000;
-        FUNC3_SRL  = 3'b101;
-        FUNC3_SLL  = 3'b001;
-        FUNC3_BEQ  = 3'b000;
-        FUNC3_BGE  = 3'b100;
-        FUNC3_BLT  = 3'b101;
-    } func3_t;
+        FUNCT3_LW   = 3'b010;
+        FUNCT3_ADDI = 3'b000;
+        FUNCT3_SW   = 3'b010;
+        FUNCT3_ADD  = 3'b000;
+        FUNCT3_SUB  = 3'b000;
+        FUNCT3_SRL  = 3'b101;
+        FUNCT3_SLL  = 3'b001;
+        FUNCT3_BEQ  = 3'b000;
+        FUNCT3_BGE  = 3'b100;
+        FUNCT3_BLT  = 3'b101;
+    } funct3_t;
 
     typedef enum logic [6:0] {
-        FUNC7_ADD  = 7'b0000000;
-        FUNC7_SUB  = 7'b0100000;
-        FUNC7_SRL  = 7'b0000000;
-        FUNC7_SLL  = 7'b0000000;
-    } func7_t;
+        FUNCT7_ADD  = 7'b0000000;
+        FUNCT7_SUB  = 7'b0100000;
+        FUNCT7_SRL  = 7'b0000000;
+        FUNCT7_SLL  = 7'b0000000;
+    } funct7_t;
+
+    typedef enum logic [3:0] {
+        ALU_ADD    = 4'b0000;
+        ALU_SUB    = 4'b0001;
+        ALU_SRL    = 4'b0010;
+        ALU_SLL    = 4'b0011;
+        ALU_CMP    = 4'b0100;
+    } alu_op_t;
     
 endpackage
