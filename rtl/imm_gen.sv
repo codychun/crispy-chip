5a4deb1a4937c4debf15b9e5e605c97198214729
codychun <codyc2023.red@gmail.com>
2026-07-12 09:57:54 -0400

    Explicit bit replication to handle signed immediates

--- rtl/imm_gen.sv
+++ rtl/imm_gen.sv
@@ -1,33 +1,39 @@
-`include "include/riscv_pkg.sv"
+`include "riscv_pkg.sv"
 
 module imm_gen(
-    input logic  [31:0] instr,
+    input  logic [31:0] instr,
+
     output logic [31:0] imm
 );
     // Opcode
-    logic [6:0] opcode;
-    assign opcode = instr[6:0];
+    riscv_pkg::opcode_t opcode;
+    assign opcode = riscv_pkg::opcode_t'(instr[6:0]);
 
     // Intermediate Immediates
     logic [31:0] imm_i;
     logic [31:0] imm_s;
     logic [31:0] imm_sb;
     logic [31:0] imm_uj;
-    // Immediates padded to 32-bits, sign preserved
-    assign imm_i  = 32'(signed'(instr[31:20]));
-    assign imm_s  = 32'(signed'({instr[31:25], instr[11:7]}));
-    assign imm_sb = 32'(signed'({instr[31], instr[7], instr[30:25], instr[11:8], 1'b0}));
-    assign imm_uj = 32'(signed'({instr[31], instr[19:12], instr[20], instr[30:21], 1'b0}));
+
+    // Immediates padded to 32-bits using explicit bit-replication
+    assign imm_i  = { {20{instr[31]}}, instr[31:20] };
+    assign imm_s  = { {20{instr[31]}}, instr[31:25], instr[11:7] };
+    assign imm_sb = { {19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0 };
+    assign imm_uj = { {11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0 };
 
     always_comb begin
+        imm = 32'b0;
+
         // MUX chooses which immediate to use based on opcode (instruction type)
         case (opcode)
-            OP_ALU_IMM: imm = imm_i;
-            OP_LOAD:    imm = imm_i;
-            OP_STORE:   imm = imm_s;
-            OP_BRANCH:  imm = imm_sb;
-            OP_JUMP:    imm = imm_uj;
-            default:    imm = 32'b0;
+            riscv_pkg::OP_ALU_IMM: imm = imm_i;
+            riscv_pkg::OP_LOAD:    imm = imm_i;
+            riscv_pkg::OP_STORE:   imm = imm_s;
+            riscv_pkg::OP_BRANCH:  imm = imm_sb;
+            riscv_pkg::OP_JUMP:    imm = imm_uj;
+
+            riscv_pkg::OP_ALU_REG: ;
+            default: ;
         endcase
     end
 
