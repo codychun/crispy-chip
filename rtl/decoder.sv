102700499ba3d36981b21ad588c47a354d435645
codychun <codyc2023.red@gmail.com>
2026-07-13 23:11:01 -0400

    Reordered output signals

--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -4,9 +4,9 @@ module decoder(
     // Instruction Fields
     output logic [6:0]  opcode,
     output logic [4:0]  rd,
-    output logic [2:0]  funct3,
     output logic [4:0]  rs1,
     output logic [4:0]  rs2,
+    output logic [2:0]  funct3,
     output logic [6:0]  funct7
 ); 
     // Unpack all possible fields simultaneously
