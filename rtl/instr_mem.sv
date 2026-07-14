a05b52e88bc4939626c7ddc70bc2f67026dbf22c
codychun <codyc2023.red@gmail.com>
2026-07-13 21:32:19 -0400

    Initial Instruction Memory ROM

---
+++ rtl/instr_mem.sv
@@ -0,0 +1,21 @@
+module instr_mem(
+    input  logic [31:0] instr_addr,
+    
+    output logic [31:0] instr_data
+);
+
+    // Allocate ROM instruction memory
+        // Byte-addressable: Max - 2^32 bytes, 2^30 words
+        // Limited to 1024 words for simulation
+    logic [31:0] rom [0:1023];
+
+    // Word-aligned asynchronous read
+        // Bottom two bits are byte offset
+    assign instr_data = rom[instr_addr[31:2]];
+
+    // Load assembly binary (hex) file
+    initial begin
+        $readmemh("program.hex", rom);
+    end
+
+endmodule
