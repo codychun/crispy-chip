58a591d44816d82d85793290d8cf40664f6fa712
codychun <codyc2023.red@gmail.com>
2026-07-13 21:31:54 -0400

    Initial Data Memory RAM

---
+++ rtl/data_mem.sv
@@ -0,0 +1,26 @@
+module data_mem(
+    input  logic        clk,
+    input  logic        mem_write_en,
+    input  logic        mem_read_en,
+    input  logic [31:0] mem_addr,
+    input  logic [31:0] mem_write_data,
+
+    output logic [31:0] mem_read_data
+);
+
+    // Allocate RAM space
+        // Byte Addressable: Max - 2^32 bytes, 2^30 words
+        // Limited to 1024 words for simulation
+    logic [31:0] ram [0:1023];
+
+    // Word-aligned asynchronous read
+    assign mem_read_data = (mem_read_en) ? ram[mem_addr[31:2]] : 32'b0;
+
+    // Synchronous write
+    always_ff @(posedge clk) begin
+        if (mem_write_en) begin
+            ram[mem_add[31:2]] <= mem_write_data;
+        end
+    end
+
+endmodule
