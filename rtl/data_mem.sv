1f422f912b04bb5ef9a8d7124d9730c886b2797a
codychun <codyc2023.red@gmail.com>
2026-08-05 10:13:13 -0400

    Changed size of RAM to accomodate data/instruction memory split

--- rtl/data_mem.sv
+++ rtl/data_mem.sv
@@ -11,7 +11,7 @@ module data_mem(
     // Allocate RAM space
         // Byte Addressable: Max - 2^32 bytes, 2^30 words
         // Limited to 1024 words for simulation
-    logic [31:0] ram [0:1023];
+    logic [31:0] ram [0:8191];
 
     // Word-aligned asynchronous read
     assign mem_read_data = (mem_read_en) ? ram[mem_addr[31:2]] : 32'b0;
@@ -19,7 +19,7 @@ module data_mem(
     // Synchronous write
     always_ff @(posedge clk) begin
         if (mem_write_en) begin
-            ram[mem_add[31:2]] <= mem_write_data;
+            ram[mem_addr[31:2]] <= mem_write_data;
         end
     end
 
