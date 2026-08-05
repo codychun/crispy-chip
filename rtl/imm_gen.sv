e55a928e9d1b3aa8893de8c6824ebca38d9961d6
codychun <codyc2023.red@gmail.com>
2026-08-05 10:15:05 -0400

    U-type immediate for AUIPC

--- rtl/imm_gen.sv
+++ rtl/imm_gen.sv
@@ -13,12 +13,14 @@ module imm_gen(
     logic [31:0] imm_i;
     logic [31:0] imm_s;
     logic [31:0] imm_sb;
+    logic [31:0] imm_u;
     logic [31:0] imm_uj;
 
     // Immediates padded to 32-bits using explicit bit-replication
     assign imm_i  = { {20{instr[31]}}, instr[31:20] };
     assign imm_s  = { {20{instr[31]}}, instr[31:25], instr[11:7] };
     assign imm_sb = { {19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0 };
+    assign imm_u  = { instr[31:12], 12'b0 };
     assign imm_uj = { {11{instr[31]}}, instr[31], instr[19:12], instr[20], instr[30:21], 1'b0 };
 
     always_comb begin
@@ -26,13 +28,13 @@ module imm_gen(
 
         // MUX chooses which immediate to use based on opcode (instruction type)
         case (opcode)
-            riscv_pkg::OP_ALU_IMM: imm = imm_i;
-            riscv_pkg::OP_LOAD:    imm = imm_i;
-            riscv_pkg::OP_STORE:   imm = imm_s;
-            riscv_pkg::OP_BRANCH:  imm = imm_sb;
-            riscv_pkg::OP_JUMP:    imm = imm_uj;
-
-            riscv_pkg::OP_ALU_REG: ;
+            riscv_pkg::OP_ALU_IMM,
+            riscv_pkg::OP_LOAD,
+            riscv_pkg::OP_JUMP_REG: imm = imm_i;
+            riscv_pkg::OP_STORE:    imm = imm_s;
+            riscv_pkg::OP_BRANCH:   imm = imm_sb;
+            riscv_pkg::OP_JUMP:     imm = imm_uj;
+            riscv_pkg::OP_AUIPC:    imm = imm_u;
             default: ;
         endcase
     end
