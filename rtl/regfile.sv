a5b6ca92eeb8350e405bf37204c13001ef6735cd
codychun <codyc2023.red@gmail.com>
2026-08-05 10:16:15 -0400

    Reset registers to 0 at start

--- rtl/regfile.sv
+++ rtl/regfile.sv
@@ -12,10 +12,17 @@ module regfile (
     // 32 registers, each 32 bits wide
     logic [31:0] registers[31:0];
 
+    // Reset all registers to 0 at the start of the simulation
+    initial begin
+        for (int i = 0; i < 32; i++) begin
+            registers[i] = 32'b0;
+        end
+    end
+
     // Connect rs1/rs2 data out to internal registers
     // Default register 0: 32'b0
-    assign rs1_data = (rs1_data == 5'b0) ? 32'b0 : registers[rs1_addr];
-    assign rs2_data = (rs2_data == 5'b0) ? 32'b0 : registers[rs2_addr];
+    assign rs1_data = (rs1_addr == 5'b0) ? 32'b0 : registers[rs1_addr];
+    assign rs2_data = (rs2_addr == 5'b0) ? 32'b0 : registers[rs2_addr];
 
     always_ff @(posedge clk) begin
         if (reg_write_en && (rd_addr != 5'b0)) begin
