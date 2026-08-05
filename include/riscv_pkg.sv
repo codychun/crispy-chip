140c1141dab4b6e727ca4b136187792e7a38b89a
codychun <codyc2023.red@gmail.com>
2026-08-05 10:04:47 -0400

    Added support for AUIPC and JALR

--- include/riscv_pkg.sv
+++ include/riscv_pkg.sv
@@ -5,13 +5,15 @@
 package riscv_pkg;
     // Instruction Opcodes
     typedef enum logic [6:0] {
-        OP_LOAD    = 7'b0000011,
-        OP_ALU_IMM = 7'b0010011,
-        OP_STORE   = 7'b0100011,
-        OP_ALU_REG = 7'b0110011,
-        OP_BRANCH  = 7'b1100011,
-        OP_JUMP    = 7'b1101111,
-        OP_SYSTEM  = 7'b1110011
+        OP_LOAD     = 7'b0000011,
+        OP_ALU_IMM  = 7'b0010011,
+        OP_STORE    = 7'b0100011,
+        OP_ALU_REG  = 7'b0110011,
+        OP_BRANCH   = 7'b1100011,
+        OP_JUMP     = 7'b1101111,
+        OP_JUMP_REG = 7'b1100111,
+        OP_AUIPC    = 7'b0010111,
+        OP_SYSTEM   = 7'b1110011
     } opcode_t;
 
     // Instruction funct3
